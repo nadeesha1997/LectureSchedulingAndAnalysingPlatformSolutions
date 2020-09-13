47b38c76c99a2d812ce14ce89238d88de46f6291
Madhushika97 <b.d.m.niroshanee.com>
2020-09-13 17:10:35 +0530

    hall building and department models added

---
+++ LectureSchedulingAndAnalysingPlatform/Models/Building.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LectureSchedulingAndAnalysingPlatform.Models
+{
+    public class Building
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Department Department { get; set; }
+    }
+}
